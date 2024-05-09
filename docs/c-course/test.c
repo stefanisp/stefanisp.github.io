770b192729fb92dbe6ed8a4e77b24bd660034f00
stefanisp <stefanispires@gmail.com>
2024-05-09 08:02:37 -0300

    Updates 2023.2

--- docs/c-course/test.c
+++ docs/c-course/test.c
@@ -2,10 +2,32 @@
 
 int main(void)
 {
+
+    char frase[20];
+    setbuf(stdin, NULL);
+printf("Digite uma frase:");
+gets(frase);
+    
+    for(int i=0; i< sizeof frase; i++){
+        printf("%d ", i);
+    }
+
+printf("%s ", frase);
+    
+
+/*
+    char string_1[10] = "Bom dia!";
+    char string_2[10];
+    string_2 = string_1;
+    
+    printf("%c", string_2[7]);
+*/
+
+
     //printf("Texto \e[4mabc");
-    printf(tgetstr("us", NULL)); /* underline on */
-printf("HELLO");
-printf(tgetstr("ue", NULL)); /* underline off */
+    //printf(tgetstr("us", NULL)); /* underline on */
+    //printf("HELLO");
+    //printf(tgetstr("ue", NULL)); /* underline off */
 
 // printf("%d", rand());
 // fflush(stdout);
