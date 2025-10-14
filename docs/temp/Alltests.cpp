8e1273ed771591e688696323a656dfd49337644c
stefanisp <stefanispires@gmail.com>
2025-10-14 18:14:25 -0300

    Curso C++: Atualizações

--- docs/temp/Alltests.cpp
+++ docs/temp/Alltests.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <string>
-#include <algorithm> // std::transform
-#include <cctype>    // std::tolower
-
 using namespace std;
-int main() { 
-     int a = 10, b = 3, c = 5;
 
-    // Operadores aritméticos
-    cout << "Soma: " << a + b << endl;
-    cout << "Subtração: " << a - b << endl;
-    cout << "Multiplicação: " << a * b << endl;
-    cout << "Divisão: " << a / b << endl;
-    cout << "Resto: " << a % b << endl;
+int main() {
+    float nota, soma = 0;
+    int quantidade = 5;
 
-    // Operadores relacionais
-    cout << "a é igual a b? " << (a == b) << endl;
-    cout << "a é maior que b? " << (a > b) << endl;
+    for (int i = 1; i <= quantidade; i++) {
+        cout << "Digite a nota " << i << ": ";
+        cin >> nota;
+        soma += nota; // soma = soma + nota
+    }
 
-    // Operadores lógicos
-    cout << "a > 5 e b < 5? " << (a > 5 && b < 5) << endl;
-    cout << "a > 5 ou b > 5? " << (a > 5 || b > 5) << endl;
-    cout << "a não é menor que b? " << !(a < b) << endl;
+    float media = soma / quantidade;
+    cout << "A média das notas é: " << media << endl;
 
-    //Operadores unários de incremento e decremento
-    cout << "Incremento pré-fixado: " << ++c << endl; // imprime 6
-    c = 5;
-    cout << "Incremento pós-fixado: " << c++ << endl;
-    
-
-     return 0;
+    return 0;
 }
