1f24ddf19cd53ba4e56a6ebba0780b0806c404a2
stefanisp <stefanispires@gmail.com>
2025-10-24 16:07:10 -0300

    Update - Vetores

--- docs/temp/Alltests.cpp
+++ docs/temp/Alltests.cpp
@@ -1,26 +1,37 @@
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// Primeira função
-void primeiraFuncao() {
-    cout << "Mensagem da primeira função." << endl;
-}
-
-// Segunda função
-void segundaFuncao() {
-    cout << "Mensagem da segunda função." << endl;
-}
-
 int main() {
-    cout << "Início do programa no main." << endl;
+    // Vetor de nomes (strings)
+    vector<string> nomes = {"Ana", "João", "José", "Maria"};
 
-    primeiraFuncao(); // chama a primeira função
+    // Vetor de notas (números reais)
+    vector<float> notas = {8.5, 7.2, 9.0, 7.8};
 
-    cout << "De volta ao main, depois da primeira função." << endl;
+    // Exibindo os dois vetores juntos
+    cout << "Lista de alunos e suas notas:" << endl;
 
-    segundaFuncao(); // chama a segunda função
-
-    cout << "Final do programa no main." << endl;
+    for (int i = 0; i < nomes.size(); i++) {
+        cout << nomes[i] << " - " << notas[i] << endl;
+    }
 
     return 0;
 }
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<string> flores;
+    vector<string> cores = {"branco", "amarelo", "azul"};
+
+    for (string cor : cores){
+        cout << cor << " - ";
+    }
+
+    return 0;
+}
+    */
