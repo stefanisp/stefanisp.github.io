22fb1d4411a59651f888b02d328863c6165536a8
stefanisp <stefanispires@gmail.com>
2025-11-28 14:43:42 -0300

    Construção do modulo 3

--- docs/temp/testes.cpp
+++ docs/temp/testes.cpp
@@ -1,15 +1,42 @@
-#include "Pessoa.hpp"
+#include <string>
 #include <iostream>
-using namespace std;
+
+class Endereco {
+private:
+    std::string rua;
+    int numero;
+
+public:
+    Endereco(std::string r, int n) : rua(r), numero(n) {}
+    
+    std::string getEnderecoCompleto(){
+        return rua + ", " + std::to_string(numero);
+    }
+
+};
+
+class Pessoa {
+private:
+    std::string nome;
+    int idade;
+    Endereco endereco;   // Composição: uma Pessoa possui um Endereco
+
+public:
+    Pessoa(std::string n, int i, Endereco e)
+        : nome(n), idade(i), endereco(e) {}
+    
+    void apresentacao(){
+        std::cout << "Olá, meu nome é " << nome << ". Tenho " << idade <<
+        " anos. \n Moro no endereço: " << endereco.getEnderecoCompleto() << std::endl;
+}
+};
+
 
 int main() {
+    Endereco end("Rua das Flores", 123);
+    Pessoa pessoa("Maria", 20, end);
 
-    Pessoa p1("Ana", 21);
-    Pessoa p2 ("Maria", 22), p3("João", 23);
-    
-    p1.apresentacao();
-    p2.apresentacao();
-    p3.apresentacao();
+    pessoa.apresentacao();
 
     return 0;
 }
